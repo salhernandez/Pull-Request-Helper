895ed231e37a3b9f5d2d40bdde76792d771cec83
Salvador Hernandez <hernandezg.sal@gmail.com>
2023-10-28 23:09:09 -0700

    Open Editor

--- replaceText.js
+++ replaceText.js
@@ -1,3 +1,39 @@
+const ckeditor5URL = 'https://cdnjs.cloudflare.com/ajax/libs/ckeditor5/40.0.0/ckeditor.min.js';
+const editorID = 'PRHELPER-editor';
+
+StringToEmoji(document);
+loadJS(ckeditor5URL, yourCodeToBeCalled, document.body);
+
+
+function loadJS(url, implementationCode, location){
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to 
+    //insert the <script> element
+
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+};
+
+function yourCodeToBeCalled(e){
+
+    // Open Editor
+    var s = document.createElement('script');
+    s.text = 
+    `
+    ClassicEditor
+        .create( document.querySelector( '#${editorID}' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+    `
+    document.getElementsByTagName('head')[0].appendChild(s);
+}
+
 function StringToEmoji(document_root) {    
 
     const reviewThread = document_root.querySelectorAll('[id^=review-thread-or-comment]')[0];
@@ -8,15 +44,14 @@ function StringToEmoji(document_root) {
     editorContainer.style.width = `${reviewThread.offsetWidth.toString()}px`;
     editorContainer.style.height = `${reviewThread.offsetHeight.toString()}px`;
     editorContainer.style.backgroundColor = 'red';
-
-
+    editorContainer.id = editorID;
+    
     reviewThread.after(wrapperDiv);
     wrapperDiv.append(reviewThread);
     wrapperDiv.append(editorContainer);
 
 }
 
-StringToEmoji(document);
 
 chrome.runtime.sendMessage({
     action: "getSource",
