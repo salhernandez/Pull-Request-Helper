df1a079a80e360ce97791a9e2b63b7c5b2f926ec
Salvador Hernandez <hernandezg.sal@gmail.com>
2023-10-31 21:44:11 -0700

    Update obj schema

--- replaceText.js
+++ replaceText.js
@@ -1,4 +1,11 @@
 const editorID = 'PRHELPER-editor';
+
+// const schema = {
+//     id: "string",
+//     type: "thread-comment" | "review-comment",
+//     blocks: []
+// }
+
 var reviewThreadIds = [];
 
 addListenerToSaveUpdatedData();
@@ -38,8 +45,8 @@ function injectScriptToWebPage() {
     const anIcon = `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
     let data = '[]'
     if (dataFromStorage) {
-        if (dataFromStorage.hasOwnProperty(reviewThreadIds[0])) {
-            data = JSON.stringify(dataFromStorage[reviewThreadIds[0]].data.blocks)
+        if (dataFromStorage.hasOwnProperty(reviewThreadIds[0].id)) {
+            data = JSON.stringify(dataFromStorage[reviewThreadIds[0].id].data.blocks)
         }
     }
     // Open Editor
@@ -149,18 +156,32 @@ function setEditorDivs(document_root) {
     const reviewComment = document_root.querySelectorAll('[data-body-version]');
     const threadComment = document_root.querySelectorAll('[id^=review-thread-or-comment]');
 
+    // const schema = {
+    //     id: "string",
+    //     type: "thread-comment" | "review-comment",
+    //     blocks: []
+    // }
+
     if (reviewComment.length) {
         finalElement = reviewComment[0];
-        reviewThreadIds.push(finalElement.id);
+        reviewThreadIds.push({
+            id: finalElement.id,
+            type: "review-comment",
+            blocks: []
+        });
     } else if (threadComment.length) {
         finalElement = threadComment[0];
-        reviewThreadIds.push(finalElement.id);
+        reviewThreadIds.push({
+            id: finalElement.id,
+            type: "thread-comment",
+            blocks: []
+        });
     } else {
+        // Nothing is found
         return;
     }
 
 
-    // data-body-version="84d62e7000dcec8774b864216601d3c44b0aa28d374da8d1c4360017f5ac5478"
     const wrapperDiv = document_root.createElement('div');
     wrapperDiv.className = "derp";
 
@@ -214,7 +235,7 @@ function addListenerToSaveUpdatedData() {
             type: "PRH_MESSAGE", options: {
                 details: {
                     ...evt.detail,
-                    id: reviewThreadIds[0]
+                    id: reviewThreadIds[0].id
                 }
             }
         });
